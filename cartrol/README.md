6472c60d0d51d84553822e8ee0b13bd9852858b5
Bill Snook <bluegoose1@mac.com>
2018-05-15 22:06:43 -0700

    Fixed Calibration page layout errors, speed slider working.

--- cartrol/README.md
+++ cartrol/README.md
@@ -3,3 +3,18 @@
 Control a home made mobile platform with Linux
 running a custom motor control program
 from a custom iOS app.
+
+Calibration
+Select speed array entry number: -7 - 0 - 7 on slider - this holds the active speed for both motors
+    Adjust left and right relatively and test until both speeds match - the car goes straight
+    Save  entry
+    Repeat for all
+
+Need:
+  Array slider plus incrementers
+  Left and right sliders
+  Test button, reverse button to return tested runs
+  Save buttons and load button
+  
+
+
--- cartrol/ViewControllers/CalibrateViewController.swift
+++ cartrol/ViewControllers/CalibrateViewController.swift
@@ -9,21 +9,23 @@
 import UIKit
 
 
-class CalibrateViewController: UIViewController {
+let SPEED_ARRAY = 8
+
+@objc class CalibrateViewController: UIViewController {
 	
 	@IBOutlet var speedSlider: UISlider!
-	@IBOutlet var speedLeftButton: UIButton!
-	@IBOutlet var speedRightButton: UIButton!
+	@IBOutlet var speedIndexIncrementButton: UIButton!
+	@IBOutlet var speedIndexDecrementButton: UIButton!
 	@IBOutlet var speedButton: UIButton!
 	@IBOutlet var saveButton: CTButton!
 	@IBOutlet var showButton: CTButton!
 	@IBOutlet var resetButton: CTButton!
 	
-	@IBOutlet var leftMotorSpeed: UISlider!
 	@IBOutlet var rightMotorSpeed: UISlider!
+	@IBOutlet var leftMotorSpeed: UISlider!
 	
 	@IBOutlet var forwardButton: CTButton!
-	@IBOutlet var setupButton: CTButton!
+	@IBOutlet var stopButton: CTButton!
 	@IBOutlet var reverseButton: CTButton!
 	
 	@IBOutlet var responseTextView: UITextView!
@@ -37,6 +39,16 @@ class CalibrateViewController: UIViewController {
 		super.viewDidLoad()
 		
 		print( "In viewDidLoad in CalibrateViewController" )
+		
+		let statusArray = targetPort.sendPi( "z\n" )
+		print( "In viewDidLoad response for status call: \(statusArray)" )
+		
+		speedSlider.value = 0
+		speedSlider.minimumValue = Float(-SPEED_ARRAY + 1)
+		speedSlider.maximumValue = Float(SPEED_ARRAY - 1)
+//		speedSlider.addTarget(self, action: Selector("indexValueChanged:"), for: .valueChanged)
+		
+		speedButton.setTitle( "\(speedIndex)", for: .normal )
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -83,33 +95,67 @@ class CalibrateViewController: UIViewController {
 	override var prefersStatusBarHidden: Bool {
 		return true
 	}
+
+	@IBAction func indexValueChanged(_ sender: UISlider) {
+		speedIndex = Int(sender.value)
+		if ( speedIndex <= -SPEED_ARRAY ) {
+			speedIndex = -SPEED_ARRAY + 1
+		}
+		if ( speedIndex >= SPEED_ARRAY ) {
+			speedIndex = SPEED_ARRAY - 1
+		}
+		speedButton.setTitle( "\(speedIndex)", for: .normal )
+		responseTextView.text = targetPort.sendPi( "j \(speedIndex)\n" )
+	}
+	
+	@IBAction func doDecrementSpeedIndex(_ sender: UIButton) {
+		speedIndex -= 1
+		if ( speedIndex <= -SPEED_ARRAY ) {
+			speedIndex += 1
+			return
+		}
+		speedSlider.value = Float(speedIndex)
+		speedButton.setTitle( "\(speedIndex)", for: .normal )
+		responseTextView.text = targetPort.sendPi( "j \(speedIndex)\n" )
+	}
+	
+	@IBAction func doIncrementSpeedIndex(_ sender: UIButton) {
+		speedIndex += 1
+		if ( speedIndex >= SPEED_ARRAY ) {
+			speedIndex -= 1
+			return
+		}
+		speedSlider.value = Float(speedIndex)
+		speedButton.setTitle( "\(speedIndex)", for: .normal )
+		responseTextView.text = targetPort.sendPi( "j \(speedIndex)\n" )
+	}
 	
 	@IBAction func doSpeedIndexIncrement(_ sender: UIButton) {
 		
 	}
 	
 	@IBAction func doSaveButtonTouch(_ sender: CTButton) {
-		responseTextView.text = targetPort.sendPi( "w\n" );
+		responseTextView.text = targetPort.sendPi( "w\n" )
 	}
 	
 	@IBAction func doShowButtonTouch(_ sender: CTButton) {
-		responseTextView.text = targetPort.sendPi( "h\n" );
+		responseTextView.text = targetPort.sendPi( "h\n" )
 	}
 	
 	@IBAction func doResetButtonTouch(_ sender: CTButton) {
-		responseTextView.text = targetPort.sendPi( "i\n" );
+		responseTextView.text = targetPort.sendPi( "i\n" )
 	}
 	
 	@IBAction func doForwardButtonTouch(_ sender: CTButton) {
-		responseTextView.text = targetPort.sendPi( "g\(speedIndex)\n" );
+		responseTextView.text = targetPort.sendPi( "g\(speedIndex)\n" )
 	}
 	
-	@IBAction func doSetupButtonTouch(_ sender: CTButton) {
-		responseTextView.text = targetPort.sendPi( "i\n" );
+	@IBAction func doStopButtonTouch(_ sender: CTButton) {
+		responseTextView.text = "s"
 	}
 	
 	@IBAction func doReverseButtonTouch(_ sender: CTButton) {
-		responseTextView.text = targetPort.sendPi( "g\(speedIndex)\n" );
+		responseTextView.text = targetPort.sendPi( "g\(speedIndex)\n" )
 	}
 	
 	@IBAction func doCancelButtonTouch(_ sender: CTButton) {
