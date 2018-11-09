019dba50d60eb475893dda9c10a82819101e4ebd
Bill Snook <bluegoose1@mac.com>
2018-11-09 09:06:20 -0800

    Car directives

--- cartrol/ViewControllers/DirectViewController.swift
+++ cartrol/ViewControllers/DirectViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class DirectViewController: UIViewController {
 	
-	
+	var isConnected = false
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -21,12 +21,82 @@ class DirectViewController: UIViewController {
 		super.viewWillAppear( animated )
 		
 		self.navigationController?.setNavigationBarHidden( false, animated: true )	// Just hide it on this page
+		
+		if targetPort.socketConnected {
+//			targetPort.sendPi( "" )	// ? Sign in ?
+			isConnected = true
+		} else {
+			// Alert
+		}
+
 	}
 
 	override func viewWillDisappear(_ animated: Bool) {
 		self.navigationController?.setNavigationBarHidden( true, animated: true )
 		
 		super.viewWillDisappear( animated )
+	}
+
+	@IBAction func tlAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 1 2" )	// Left
+		targetPort.sendPi( "1 1 4" )	// Right
+	}
+	
+	@IBAction func topAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 1 4" )	// Left
+		targetPort.sendPi( "1 1 4" )	// Right
+	}
+
+	@IBAction func trAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 1 4" )	// Left
+		targetPort.sendPi( "1 1 2" )	// Right
+	}
+
+	@IBAction func leftAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 0 3" )	// Left
+		targetPort.sendPi( "1 1 3" )	// Right
+	}
+
+	@IBAction func centerAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "s" )
+	}
+
+	@IBAction func rightAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 1 3" )	// Left
+		targetPort.sendPi( "1 0 3" )	// Right
+	}
+
+	@IBAction func blAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 1 2" )	// Right
+		targetPort.sendPi( "1 1 4" )	// Right
+	}
+
+	@IBAction func bottomAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 1 4" )	// Right
+		targetPort.sendPi( "1 1 4" )	// Right
+	}
+
+	@IBAction func brAction(_ sender: UIButton) {
+		guard isConnected else { print( "Not connected!" ); return }
+		
+		targetPort.sendPi( "0 1 4" )	// Left
+		targetPort.sendPi( "1 1 2" )	// Right
 	}
 
 }
